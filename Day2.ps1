9f6a5cf9745a39bbefa8e9a7bd593ebcb24f3c78
Kaufman <jdkaufman@ku.edu>
2019-12-02 22:12:37 -0600

    day 2 not working yet...

--- Day2.ps1
+++ Day2.ps1
@@ -1,10 +1,42 @@
-$opcodes = Get-Content day2.txt 
+$opcodes = ((Get-Content day2.txt).split(",")).forEach({[int]$psitem})
 
-$opcodes = 1,9,10,3,2,3,11,0,99,30,40,50
+#$opcodes = 1,9,10,3,2,3,11,0,99,30,40,50
 
 $i=0  
-ForEach($opcode in ($opcodes[$i]..$opcodes[$i+4])){
+Do{
+    Switch($opcodes[$i]){
 
-    Write $opcode
+       1 {  
 
-}
+            $x = $opcodes[$i+1] 
+
+            $y = $opcodes[$i+2]
+
+            $j = $opcodes[$x] + $opcodes[$y]
+
+            $z = $opcodes[$i+3]
+            
+            $opcodes.Item($z) = $j 
+
+            $i = $i + 4
+
+            }
+
+       2 {  
+
+            $j = ($opcodes[$opcodes[$i+1]] * $opcodes[$opcodes[$i+2]])
+
+            $opcodes.Item($opcodes[$i+3]) = $j 
+
+            $i = $i + 4
+
+            }
+      99 { 
+
+          Write-Output $opcodes[0]
+
+          throw }
+
+
+    }  
+}While($i -lt $opcodes.Count)
